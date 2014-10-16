c2b2f30424f3f4b4a4a870fffef6a001a3ba3b10
Uko <m.gregoriades@gmail.com>
2014-10-16 22:43:04 +0200

    Update utility.sh

--- utility.sh
+++ utility.sh
@@ -3,5 +3,16 @@
 # utility script
 #
 # m.gregoriades@gmail.com
-#
-# - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+function log {
+  type="$1"
+  message="$2"
+  
+  [[ $type ]] || { echo "no type=> aborting..."; return 1; }
+  [[ $message ]] || { echo "no message=> aborting..."; return 1; }
+  
+  [[ $LOG ]] && echo "$(date +%Y-%m-%d %H:%M:%S) $type $message" > $LOG
+  [[ $LOG ]] || echo "$(date +%Y-%m-%d %H:%M:%S) $type $message"
+  
+  return 0
+}
